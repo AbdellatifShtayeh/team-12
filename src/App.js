40f7aaee2a9187e1317eb35834815588573380cb
masa <masa@egmail.com>
2023-12-15 00:36:39 +0200

    Navbar task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import NavBar from "./components/NavBar";
 function App() {
-  return (
-    <>
-    
-    </>
+  return ( 
+    <div className="App">
+      <NavBar />
+      <div className="rest"></div>
+    </div>    
   );
 }
 
 export default App;
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./NavBar.css";
+import { InputText } from "primereact/inputtext";
+import { Menubar } from "primereact/menubar";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+import { Button } from "primereact/button";
+import { Badge } from "primereact/badge";
+import { Avatar } from "primereact/avatar";
+import { TfiAnnouncement } from "react-icons/tfi";
+function NavBar(){
+  const items = [
+    {
+      label: "Demo",
+      items: [],
+    },
+    {
+      label: "Pages",
+      items: [],
+    },
+    {
+      label: "Account",
+      items: [],
+    },
+    {
+      label: "My Network",
+    },
+  ];
+  return (
+    <div className="nav-bar-container">
+      <div className="container">
+        <div className="left-side">
+          <Button
+            onClick={() => {
+              window.location.reload();
+            }}
+            icon={<TfiAnnouncement size={25} />}
+          ></Button>
+          <InputText placeholder="Search" type="text" className="w-full" />
+        </div>
+        <div className="right-side">
+          <Menubar model={items} className="menu" />
+          <Button icon="pi pi-comment" />
+          <Button icon="pi pi-cog" />
+          <Avatar
+            className="p-overlay-badge"
+            icon="pi pi-bell"
+            size="large"
+            id="bell"
+          >
+            <Badge severity="danger" />
+          </Avatar>
+          <Avatar
+            className="p-overlay-badge profile"
+            image="https://designyourownblog.com/wp-content/uploads/2020/11/create-perfect-profile-photo-2em.jpg"
+            size="large"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default NavBar;
+
+
+
+
