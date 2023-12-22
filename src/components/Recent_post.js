0329a59adf44e4ea5f64c5eae49d17738aa7aee6
Ebrwa <140380078+Ebrwa@users.noreply.github.com>
2023-12-22 19:13:24 +0300

    Final task

---
+++ src/components/Recent_post.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Tasck.css';
+function Tasck1() {
+  return (
+    <div>
+    <div class="test">
+
+<div className="frame" >
+  <div className="card" style={{width: '28rem'}}>
+    <div className="card-body ">
+      <h3 className="card-title">Recent post</h3>
+    </div>
+    <div className="post">
+      <div className="post-title">Ten questions you should answer <br />truthfully</div>
+      <div className="timestamp">2hr</div>
+    </div>
+    <div className="post">
+      <div className="post-title">Five unbelievable facts about money</div>
+      <div className="timestamp">3hr</div>
+    </div>
+    <div className="post">
+      <div className="post-title">Best Pinterest Boards for learning about<br /> business</div>
+      <div className="timestamp">4hr</div>
+    </div>
+    <div className="post">
+      <div className="post-title">Skills that you can learn from business</div>
+      <div className="timestamp">6hr</div>
+      <div className="view-all"><font size="16"  color="#b0bec5" >... </font>View all latest news</div>
+    </div>
+ 
+  </div> </div>
+
+
+
+    </div>
+
+    </div>
+
+  )
+}
+
+export default Tasck1
--- src/components/leave_reply.jsx
+++ src/components/leave_reply.jsx
@@ -1,31 +1,64 @@
+import { useState } from 'react';
 import '../styles/leave_reply.css';
 
-export function LeaveReply(props) {
-  return (
+export default function LeaveReply(props) {
+  let [data, setData] = useState({
+    name: '',
+    email: '',
+    comment: '',
+  })
+
+  function handlePost() {
+    if(props.onSubmit) {
+      props.onSubmit(data)
+    }
+    setData({
+      name: '',
+      email: '',
+      comment: '',
+    })
+  }
+  return ( 
     <div className="reply_container">
       <div className="reply_box">
         <h1>Leave a reply</h1>
         <div className="reply_row">
             <div className="reply_item">
                 <span>Name *</span>
-                <input className='reply_input' type="text" />
+                <input 
+                  className='reply_input' 
+                  type="text"
+                  value={data.name}
+                  onChange={(e) => setData({...data, name: e.target.value})}
+                />
             </div>
             <div className="reply_item">
                 <span>Email *</span>
-                <input className='reply_input' type="email" />
+                <input 
+                  className='reply_input' 
+                  type="email"
+                  value={data.email}
+                  onChange={(e) => setData({...data, email: e.target.value})}
+                />
             </div>
         </div>
         <div className="reply_row">
             <div className="reply_item">
                 <span>Your Comment *</span>
-                <textarea className='reply_textarea'></textarea>
+                <textarea 
+                className='reply_textarea'
+                value={data.comment}
+                onChange={(e) => setData({...data, comment: e.target.value})}
+                >
+                {data.comment}
+                </textarea>
             </div>
         </div>
         <div className="check">
             <input type="checkbox" />
             <span>Save my name and email in this browser for the next time I comment.</span>
         </div>
-        <button className='reply_button'>Post comment</button>
+        <button onClick={handlePost} className='reply_button'>Post comment</button>
       </div>
     </div>
   );
