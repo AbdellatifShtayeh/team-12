5da1490aadc3f75f672e492e4d53979be285b840
AbdellatifShtayeh <s12220214@stu.najah.edu>
2023-12-22 16:22:32 +0200

    fix the app styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { collection, getDocs } from "firebase/firestore";
 import db from "../src/config/firebase.js";
 import Blog_preview from '../src/components/blog_preview.js'
 import Pagination from "./components/pagination.js";
-import NavBar from "../src/config/NavBar.js" 
+import NavBar from "../src/config/NavBar.js"
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min.js";
 
@@ -17,49 +17,53 @@ const pquery = collection(db, "blog-preview content page 2");
 const pquerySnapshot = await getDocs(pquery);
 const page2content = pquerySnapshot.docs.map(doc => doc.data());
 
-  
+
 
 function App() {
 
   return (
     <>
       <Router>
-        <NavBar />
-        {/* content alligning for the first page */}
-        <div className='container'>
-          <div className="col-lg-8">
-            <div className="bg-mode p-4">
-              {/* starting of blogs */}
-              <Switch>
-                <Route exact path='/page-1'>
-                  {documents.map(doc => (
+        <div style={{backgroundColor:'#eff2f6'}}>
+          <div style={{ backgroundColor: 'white' }}>
+            <NavBar />
+          </div>
+          {/* content alligning for the first page */}
+          <div className='container' >
+            <div className="col-lg-8" >
+              <div className="bg-mode p-4" style={{backgroundColor:'white'}}>
+                {/* starting of blogs */}
+                <Switch>
+                  <Route exact path='/page-1'>
+                    {documents.map(doc => (
 
-                    <Blog_preview
-                      key={doc.id} heading={doc.heading} description={doc.description} tag={doc.tag}
-                      tagColor={doc.tagColor} tagBg={doc.tagBg} timeStamp={doc.timeStamp} img={doc.img}
-                    />
-                  ))}
-                </Route>
-                {/* ending of blogs page 1 */}
-                {/* page 2 */}
-                <Route path='/page-2'>
-                  {page2content.map(doc => (
+                      <Blog_preview
+                        key={doc.id} heading={doc.heading} description={doc.description} tag={doc.tag}
+                        tagColor={doc.tagColor} tagBg={doc.tagBg} timeStamp={doc.timeStamp} img={doc.img}
+                      />
+                    ))}
+                  </Route>
+                  {/* ending of blogs page 1 */}
+                  {/* page 2 */}
+                  <Route path='/page-2'>
+                    {page2content.map(doc => (
 
-                    <Blog_preview
-                      key={doc.id} heading={doc.heading} description={doc.description} tag={doc.tag}
-                      tagColor={doc.tagColor} tagBg={doc.tagBg} timeStamp={doc.timeStamp} img={doc.img}
-                    />
-                  ))}
-                </Route>
-              </Switch>
-              {/* page navigation */}
-              <Pagination />
+                      <Blog_preview
+                        key={doc.id} heading={doc.heading} description={doc.description} tag={doc.tag}
+                        tagColor={doc.tagColor} tagBg={doc.tagBg} timeStamp={doc.timeStamp} img={doc.img}
+                      />
+                    ))}
+                  </Route>
+                </Switch>
+                {/* page navigation */}
+                <Pagination />
+              </div>
             </div>
           </div>
         </div>
       </Router>
 
-    
+
 
     </>
   );
